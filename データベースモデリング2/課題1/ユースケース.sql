cc882ce0bf178b42db30acb825bf98a891741347
zyumax <kazuma_sge@icloud.com>
2025-03-02 08:59:40 +0900

    横断検索機能実装

--- データベースモデリング2/課題1/ユースケース.sql
+++ データベースモデリング2/課題1/ユースケース.sql
@@ -17,3 +17,15 @@ SELECT user_id, COUNT(messages.id) FROM messages GROUP BY user_id;
 -- ワークスペースごとの参加ユーザー
 SELECT ws.workspace_id as "ワークスペース名", COUNT(ws.user_id) "ユーザー数" FROM workspaces_users as ws
 GROUP BY ws.workspace_id;
+
+-- 横断検索機能(メッセージとスレッドメッセージを横断的に検索できること（例えば「hoge」と検索したら、この文字列を含むメッセージとスレッドメッセージを両方とも取得できること）
+WITH
+ workspaces_channels AS (
+  SELECT cu.channel_id FROM channels_users cu
+  JOIN channels c ON cu.channel_id = c.id
+  JOIN workspaces w ON c.workspace_id = w.id
+  WHERE cu.user_id = 1 AND w.id = 1
+ )
+SELECT m.user_id, m.post, m.channel_id FROM messages m
+JOIN workspaces_channels wc ON m.channel_id = wc.channel_id
+WHERE MATCH(post) AGAINST('こんばんは' IN NATURAL LANGUAGE MODE);
