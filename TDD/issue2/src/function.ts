fdde9ba996c438d03732ef483b3d7b9c86a65b77
zyumax <kazuma_sge@icloud.com>
2025-11-08 21:55:48 +0900

    テスト作成

--- TDD/issue2/src/function.ts
+++ TDD/issue2/src/function.ts
@@ -1,60 +1,70 @@
-export function add(args: number[]): number | string {
+export function validCountNumber(args: number[]) {
   if(args.length === 0) {
     throw new Error("引数を指定してください");
   }  
   if(args.length >= 31) {
     throw new Error("引数は30個以下にしてください");
   }
-  const total: number = args.reduce(
+}
+
+export function add(args: number[]): number | string {
+  validCountNumber(args);
+
+  const result: number = args.reduce(
     (acc, cur) => acc + cur, 0
   );
-  if (total > 1000) {
+  if (result > 1000) {
     return 'too big';
   }
-  return total;
+  return result;
 };
 
 export function substract(args: number[]): number | string {
-  if(args.length === 0) {
-    throw new Error("引数を指定してください");
-  }  
-  if(args.length >= 31) {
-    throw new Error("引数は30個以下にしてください");
-  }
-  const total = args.reduce((acc, cur) => acc - cur);
-  if (total < 0) {
+  validCountNumber(args);
+
+  const result: number = args.reduce((acc, cur) => acc - cur);
+  if (result < 0) {
     return 'negative number'
   }
-  return total;
+  return result;
 };
 
 export function multi(args: number[]): number | string {
-  if(args.length === 0) {
-    throw new Error("引数を指定してください");
-  }  
-  if(args.length >= 31) {
-    throw new Error("引数は30個以下にしてください");
-  }
-  const total = args.reduce((acc, cur) => acc * cur);
-  if (total > 1000) {
+  validCountNumber(args);
+
+  const result: number = args.reduce((acc, cur) => acc * cur);
+  if (result > 1000) {
     return 'big big number';
   }
-  return total;
+  return result;
 };
 
 export function divide(args: number[]): number | string {
-  if(args.length === 0) {
-    throw new Error("引数を指定してください");
-  }  
-  if(args.length >= 31) {
-    throw new Error("引数は30個以下にしてください");
-  }
+  validCountNumber(args);
 
   const result: number = args.reduce((acc, cur) => {
     if (cur === 0) throw new Error("0で割ることはできません");
     return acc / cur
   });
-  const getNum = Number.isInteger(result) ? result : Math.round(result * 10) / 10;
+  const round1dp = Number.isInteger(result) ? result : Math.round(result * 10) / 10;
 
-  return getNum;
+  return round1dp;
 };
+
+const funcName = process.argv[2];
+const numbers = process.argv.slice(3).map(Number);
+
+let result
+if (funcName === 'add') {
+  result = add(numbers);
+} else if (funcName === 'substract') {
+  result = substract(numbers);
+} else if (funcName === 'multi') {
+  result = multi(numbers);
+} else if (funcName === 'divide') {
+  result = divide(numbers);
+} else {
+  result = '関数を定義してください'
+}
+
+console.log(funcName, numbers, result);
--- TDD/issue2/tests/funtion.test.ts
+++ TDD/issue2/tests/funtion.test.ts
@@ -1,7 +1,4 @@
-import {add} from '../src/function';
-import {substract} from '../src/function';
-import {multi} from '../src/function';
-import {divide} from '../src/function';
+import {add, substract, multi, divide, validCountNumber} from '../src/function';
 
 describe('足し算のテスト', () => {
   test('2つの数字を足す', () => {
@@ -112,5 +109,98 @@ describe('割り算のテスト', () => {
       // Act & Assert
       expect(() => divide([])).toThrow(new Error('引数を指定してください'));
       expect(() => divide([])).toThrow('引数を指定してください');
+  });
+});
+
+describe('共通化のテスト', () => {
+  describe('共通部分', () => {
+    test('引数なしのエラー', () => {
+      expect(() => validCountNumber([])).toThrow(new Error('引数を指定してください'));
+    });
+    test('引数が31個以上はエラー', () => {
+      const arr = new Array(31).fill(1);
+      expect(() => validCountNumber(arr)).toThrow('引数は30個以下にしてください');
+    });
+    test('引数が30個まではOK', () => {
+      const arr = new Array(30).fill(1);
+      expect(() => validCountNumber(arr)).not.toThrow();
+    });
+  });
+  describe('addの部分', () => {
+    test('addの確認', () => {
+      expect(add([2,3])).toBe(5);
+    });
+    test('addが1000の場合は1000を返す', () => {
+      expect(add([600,400])).toBe(1000);
+    });
+    test('足し算の結果が1000を超える場合、too bigを返す', () => {
+      // Act & Assert
+      expect(add([1000,3,4])).toBe('too big');
+    });
+    test('addで引数が31個以上はエラー', () => {
+      const arr = new Array(31).fill(1);
+      expect(() => add(arr)).toThrow('引数は30個以下にしてください');
+    });
+    test('addで引数なしのエラー', () => {
+      expect(() => add([])).toThrow(new Error('引数を指定してください'));
+    });
+  });
+  describe('substractの部分', () => {
+    test('2つの数字を引く', () => {
+      expect(substract([5,3])).toBe(2)
+    });
+    test('3つの数字を引く', () => {
+      expect(substract([5,3,1])).toBe(1);
+    });
+    test('計算結果がマイナスの場合、negative numberを返す', () => {
+      expect(substract([1, 3])).toBe('negative number')
+    });
+    test('substractで引数が31個以上はエラー', () => {
+      const arr = new Array(31).fill(1);
+      expect(() => substract(arr)).toThrow('引数は30個以下にしてください');
+    });
+    test('substractで引数なしのエラー', () => {
+      expect(() => substract([])).toThrow(new Error('引数を指定してください'));
+    });
+  });
+  describe('multipleの部分', () => {
+    test('2つの数字をかける', () => {
+      expect(multi([5,3])).toBe(15)
+    });
+    test('3つの数字をかける', () => {
+      expect(multi([5,3,2])).toBe(30);
+    });
+    test('1000以上の計算結果の場合、big big numberを返す', () => {
+      expect(multi([100, 20])).toBe('big big number');
+    });
+    test('multiで引数が31個以上はエラー', () => {
+      const arr = new Array(31).fill(1);
+      expect(() => multi(arr)).toThrow('引数は30個以下にしてください');
+    });
+    test('multiで引数なしのエラー', () => {
+      expect(() => multi([])).toThrow(new Error('引数を指定してください'));
+    });
+  });
+  describe('divideの部分', () => {
+    test('2つの数字の割り算', () => {
+      expect(divide([4,2])).toBe(2);
+    });
+    test('3つの数字の割り算', () => {
+      expect(divide([30,5,2])).toBe(3);
+    });
+    test('割り切れない場合の割り算', () => {
+      expect(divide([10,3])).toBe(3.3);
+    });
+    test('0で除算した場合にエラーを返す', () => {
+      expect(() => divide([10,0])).toThrow(new Error("0で割ることはできません"))
+      expect(() => divide([10,0])).toThrow('0で割ることはできません');
+    });
+    test('multiで引数が31個以上はエラー', () => {
+      const arr = new Array(31).fill(1);
+      expect(() => divide(arr)).toThrow('引数は30個以下にしてください');
+    });
+    test('multiで引数なしのエラー', () => {
+      expect(() => divide([])).toThrow(new Error('引数を指定してください'));
+    });
   });
 });
