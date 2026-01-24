1bc93d5d16bf247988777b75b4367eebde3e4845
zyumax <kazuma_sge@icloud.com>
2026-01-24 20:28:04 +0900

    設計原則_課題3

---
+++ DesignPrinciples/issue3/issue3.ts
@@ -0,0 +1,26 @@
+// privateで、外部からアクセスできないように変更
+class Person {
+    private name: string
+    private starWorkingAt: Date
+    constructor(name: string, startWorkingAt: Date) {
+        this.name = name
+        this.starWorkingAt = startWorkingAt
+    }
+}
+
+class Company {
+    private people: Person[]
+    // 外部から書き換えられないように参照コピー
+    constructor(people: Person[]) {
+        this.people = [...people];
+    }
+
+    get getPeople(): readonly Person[] {
+        return this.people;
+    }
+
+    // personが増えたとき用のメソッドを追加
+    addPerson(person: Person) {
+        this.people.push(person);
+    }
+}
